d1e3e74b71b15627138547c4dc8f026f41e8ecf2
idea <idea@idea.local>
2018-04-02 16:24:18 +0800

    Initial Commit

---
+++ OC_CL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_CL
+//
+//  Created by idea on 2018/4/2.
+//  Copyright © 2018年 idea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
